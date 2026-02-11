d6c45481b1fa79d4708d9b76dab591ebcbe38017
Whatever-Works123 <eaton.garrett@icloud.com>
2026-02-11 13:10:19 -0600

    Update games-config.js

--- games-config.js
+++ games-config.js
@@ -1,13 +1,12 @@
 // This is where you add new games.
 // Just copy the structure in the curly brackets {}, add a comma, and fill in the new info.
 
-const games = [
+  const games = [
   {
-    id: "asteroids",
-    title: "Asteroids HTML5",
-    description: "Classic space shooter. Watch out for floating rocks!",
-    folder: "asteroids", // Matches the folder name inside /games/
-    thumbnail: "assets/asteroids-thumb.png" // You can create a screenshot for this
+    id: "crappybird",
+    title: "CrappyBird",
+    folder: "crappybird", // Matches the folder name inside /games/
+    thumbnail: "blank" // You can create a screenshot for this
   },
   // Example of how to add a second game later:
   // {
